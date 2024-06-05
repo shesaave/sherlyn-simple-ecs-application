88fdb6cd307f3eb9409c945f8b614114f49563fe
Reecha Khanal <136762730+amazreech@users.noreply.github.com>
2024-06-05 12:43:13 -0400

    Create fetch_stats.py

---
+++ .github/workflows/fetch_stats.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import os
+
+GITHUB_API_URL = "https://api.github.com"
+REPO_OWNER = os.getenv('REPO_OWNER')
+REPO_NAME = os.getenv('REPO_NAME')
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+
+headers = {
+  "Authorization":f"token {GITHUB_TOKEN}"
+}
+
+url = f"{GITHUB_API_URL}/repos/{REPO_OWNER}/{REPO_NAME}/stats/contributors"
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+  stats = response.json()
+  with open('repo_stats.json', 'w') as f:
+    json.dump(stats, f)
+else:
+  print(f"Failed to fecth data: {response.status_code}")
+  exit(1)
