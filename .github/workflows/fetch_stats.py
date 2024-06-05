91c8156113f61dd596442f128fa79f3c3438afc3
Reecha Khanal <136762730+amazreech@users.noreply.github.com>
2024-06-05 13:23:00 -0400

    Update fetch_stats.py

--- .github/workflows/fetch_stats.py
+++ .github/workflows/fetch_stats.py
@@ -13,18 +13,20 @@ headers = {
 
 url = f"{GITHUB_API_URL}/repos/amazreech/{REPO_NAME}/stats/contributors"
 print(f"URL: {url}")
-response = requests.get(url, headers=headers)
-print(f"response: {response}")
 
-if response.status_code == 202:
-  print("Got 202, Wating...")
-  time.sleep(30)
+while True:
   response = requests.get(url, headers=headers)
-
-if response.status_code == 200:
-  stats = response.json()
-  with open('repo_stats.json', 'w') as f:
-    json.dump(stats, f)
-else:
-  print(f"Failed to fecth data: {response.status_code}")
-  exit(1)
+  
+  if response.status_code == 200:
+    stats = response.json()
+    with open('repo_stats.json', 'w') as f:
+      json.dump(stats, f)
+    break
+    
+  elif response.status_code == 202:
+    print("Got 202, Wating...")
+    time.sleep(30)
+    
+  else:
+    print(f"Failed to fecth data: {response.status_code}")
+    break
