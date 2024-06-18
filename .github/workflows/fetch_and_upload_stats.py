ea1e60c5e82afe0fa287fe97b18b64af63964609
Reecha Khanal <136762730+amazreech@users.noreply.github.com>
2024-06-18 16:24:27 -0400

    Update fetch_and_upload_stats.py

--- .github/workflows/fetch_and_upload_stats.py
+++ .github/workflows/fetch_and_upload_stats.py
@@ -73,36 +73,63 @@ def fetch_num_closed_prs_yesterday():
 
 def upload_metrics_to_cloudwatch(num_issues, num_prs_open, num_prs_closed_yesterday):
 
-  if num_issues is not None and num_prs_open is not None:
-    # put metric data to CloudWatch
-    print(f"Number of PRs: {num_prs_open}")
-    print(f"Number of Issues: {num_issues}")
-    response = client.put_metric_data(
-      Namespace = NAMESPACE,
-      MetricData = [
-        {
-          'MetricName':f'NumberOfOpenIssues.{REPO_NAME}',
-          'Value':num_issues,
-          'Unit':'Count'
-        },
-        {
-          'MetricName':f'NumberOfOpenPRs.{REPO_NAME}',
-          'Value':num_prs_open,
-          'Unit':'Count'
-        },
-        {
-          'MetricName':f'NumberOfPRsClosed.{REPO_NAME}',
-          'Value':num_prs_closed_yesterday,
-          'Unit':'Count'
-        }
-      ]
-    )
+  for metric_name, value in metrics.items():
+    metric_data.append({
+      'MetricName':metric_name,
+      'Value':value,
+      'Unit':'Count',
+      'Timestamp':metrics['Timestamp']
+    })
+    response = client.put_metric_data(Namespace = NAMESPACE, MetricData = metric_data)
     print("Uploaded metrics to cloudwatch:", response)
-  else:
-    print("No metrics to Upload")
+
+  # if num_issues is not None and num_prs_open is not None:
+  #   # put metric data to CloudWatch
+  #   print(f"Number of PRs: {num_prs_open}")
+  #   print(f"Number of Issues: {num_issues}")
+  #   response = client.put_metric_data(
+  #     Namespace = NAMESPACE,
+  #     MetricData = [
+  #       {
+  #         'MetricName':f'NumberOfOpenIssues.{REPO_NAME}',
+  #         'Value':num_issues,
+  #         'Unit':'Count'
+  #       },
+  #       {
+  #         'MetricName':f'NumberOfOpenPRs.{REPO_NAME}',
+  #         'Value':num_prs_open,
+  #         'Unit':'Count'
+  #       },
+  #       {
+  #         'MetricName':f'NumberOfPRsClosed.{REPO_NAME}',
+  #         'Value':num_prs_closed_yesterday,
+  #         'Unit':'Count'
+  #       }
+  #     ]
+  #   )
+  #   print("Uploaded metrics to cloudwatch:", response)
+  # else:
+  #   print("No metrics to Upload")
 
 if __name__ == "__main__":
-  num_open_issues = fetch_num_open_issues()
-  num_open_prs = fetch_num_open_prs()
-  num_closed_prs = fetch_num_closed_prs_yesterday()
-  upload_metrics_to_cloudwatch(num_open_issues - num_open_prs, num_open_prs, num_closed_prs)
+  if len(sys.argv != 3):
+    print("Usage: python fetch_and_upload_stats.py <start_date> <end_date>")
+    sys.exit(1)
+
+  start_date = datetime.fromisoformat(sys.argv[1])
+  end_date = datetime.fromisoformat(sys.argv[1])
+
+  current_date = start_date
+  while current_date <= end_date:
+    num_open_issues = fetch_num_open_issues()
+    num_open_prs = fetch_num_open_prs()
+    num_closed_prs = fetch_num_closed_prs_yesterday()
+
+    metrics = {
+      'NumberOfOpenIssues:' num_open_issues - num_open_prs,
+      'NumberOfOpenPRs:' num_open_prs
+      'Timestamp': current_date
+    }
+    
+    upload_metrics_to_cloudwatch(metrics)
+    current_date += timedelta(days=1)
